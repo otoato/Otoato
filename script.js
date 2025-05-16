57f8ac22b7a4e25cb5d88776f041dc811416817c
otoato <otoato.art@gmail.com>
2025-05-16 15:40:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// 今後、音や地図を追加できます
+fetch('data/posts.json')
+  .then(response => response.json())
+  .then(data => {
+    const gallery = document.getElementById('gallery');
+    data.posts.forEach(post => {
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+        <a href="detail.html?id=${post.id}">
+          <img src="${post.image}" alt="${post.title}">
+        </a>
+        <h3>${post.title}</h3>
+        <p>${post.location}</p>
+      `;
+      gallery.appendChild(card);
+    });
+  });
